a15e46ee0321f31a732f02ad04e3891f2e2633b3
unknown <sgbtech96@gmail.com>
2020-02-19 22:39:26 +0530

    added

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-
+var passportLocalMongoose = require('passport-local-mongoose');
 var UserSchema = new mongoose.Schema({
 name :{type:String},
 rollno :{type:String},
@@ -9,5 +9,5 @@ email : {type:String},
 username : {type:String},
 collagename : {type:String}
 });
-
+UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
