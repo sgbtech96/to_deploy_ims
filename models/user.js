312d26218e8cfe2255c393cbfce40f7448fe21d3
ChandanSitlani <40667874+ChandanSitlani@users.noreply.github.com>
2020-02-22 16:08:19 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,12 +2,11 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require('passport-local-mongoose');
 var UserSchema = new mongoose.Schema({
 name :{type:String},
-rollno :{type:String},
+phone :{type:String},
 role : {type:String},
-age : {type:Number},
 email : {type:String},
 username : {type:String},
-collagename : {type:String}
+thana : {type:String}
 });
 UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
